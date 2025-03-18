fef6baed7a31f814f8b5a34a0ce0f4cf9159ba34
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 07:01:32 +0700

    first commit

---
+++ PemrosesData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmodul5_103022300104
+{
+    internal class PemrosesData<T>
+    {
+        private T data;
+        public PemrosesData(T data)
+        {
+            this.data = data;
+        }
+
+        public void DapatkanNilaiTerbesar(T data)
+        {
+
+        }
+    }
+}
---
+++ SimpleDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmodul5_103022300104
+{
+    internal class SimpleDataBase
+    {
+
+    }
+}
